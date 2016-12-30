c228a216732eeafa1f049aa334a6fd1846c981d3
d_kulinichev <kulinichev.dmitry@yandex.ru>
2016-12-30 12:46:34 +0200

    some changes

--- src/com/test/task_6.java
+++ src/com/test/task_6.java
@@ -1,29 +1,34 @@
 package com.test;
 
-/**
+/*****************************************************************
+ *
  * Задача 6
  * Создать двумерный массив из 8 строк по 5 столбцов в каждой
  * из случайных целых чисел из отрезка *10;99+.
  * Вывести массив на экран
- */
+ *
+ *****************************************************************/
 
 public class task_6 {
     public static void main(String[] args) {
-        // Помещаем в константы исходные данные
+
+        // Помещаем исходные данные в константы
         final int ROWS_COUNT=8;
         final int COLS_COUNT=5;
-        // Создаем двумерный массив и наполняем его элементами
-        int[][] nums= new int[ROWS_COUNT][COLS_COUNT];
+
+
+        // Объявляем и инициализируем двумерный массив и наполняем его элементами
+        int[][] numbers= new int[ROWS_COUNT][COLS_COUNT];
         for (int r = 0; r<ROWS_COUNT; r++) {
             for (int c = 0; c<COLS_COUNT; c++) {
-                nums[r][c]= (int)(Math.random()*10+99);   ///  надо почитать про функцию!!!! и параметры
+                numbers[r][c]= (int)(Math.random()*10+99);
             }
         }
-        // Выводим  результат
-        for (int r = 0; r<=nums.length-1; r++) {
+        // Формируем и выводим результат
+        for (int r = 0; r<=numbers.length-1; r++) {
             String row="";
-            for (int c = 0; c<=nums[r].length-1; c++) {
-                row+=nums[r][c]+" ";
+            for (int c = 0; c<=numbers[r].length-1; c++) {
+                row+=numbers[r][c]+" | ";
             }
             System.out.println(row);
         }
